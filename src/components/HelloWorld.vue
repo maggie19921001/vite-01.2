bde3c3cb70b907dee836150b47b2e0135ca5d0e4
yuhuei <maggie19921001@gmail.com>
2024-07-31 17:03:01 +0800

    restart

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup> //自帶setup及return
+  import { ref } from 'vue';
+  
+  const num = ref(2);
+  function clickMe(){
+    num.value++
+  }
+</script>
+
+<template>
+  <div>
+    {{ num }}
+    <button type="button" v-on:click="clickMe">點擊</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
